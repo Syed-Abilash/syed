f01e9b884a954cf0703def49364ca8d302186628
Syed-Abilash <iquwansyed09@gmail.com>
2024-08-25 15:42:34 +0530

    protfolio

---
+++ js/google-map.js
@@ -0,0 +1,62 @@
+
+var google;
+
+function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
+    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    // 39.399872
+    // -8.224454
+    
+    var mapOptions = {
+        // How zoomed in you want the map to start at (always required)
+        zoom: 7,
+
+        // The latitude and longitude to center the map (always required)
+        center: myLatlng,
+
+        // How you would like to style the map. 
+        scrollwheel: false,
+        styles: [
+            {
+                "featureType": "administrative.country",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    },
+                    {
+                        "hue": "#ff0000"
+                    }
+                ]
+            }
+        ]
+    };
+
+    
+
+    // Get the HTML DOM element that will contain your map 
+    // We are using a div with id="map" seen below in the <body>
+    var mapElement = document.getElementById('map');
+
+    // Create the Google Map using out element and options defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+    
+    var addresses = ['Tamil Nadu'];
+
+    for (var x = 0; x < addresses.length; x++) {
+        $.getJSON('https://www.google.com/maps/place/JB+Beef+Stall/@10.9579453,76.9808208,17z/data=!3m1!4b1!4m6!3m5!1s0x3ba85b823667b8cf:0x3ce2c5f7d8a428b5!8m2!3d10.95794!4d76.9833957!16s%2Fg%2F11tmrwbwgx?entry=ttu'+addresses[x]+'&sensor=false', null, function (data) {
+            var p = data.results[0].geometry.location
+            var latlng = new google.maps.LatLng(p.lat, p.lng);
+            new google.maps.Marker({
+                position: latlng,
+                map: map,
+                icon: 'images/loc.png'
+            });
+
+        });
+    }
+    
+}
+google.maps.event.addDomListener(window, 'load', init);
